568c804eea5979945389d82bd60b9a65437086bc
ana26steysha <anastasiadontsu26@gmail.com>
2023-07-24 15:00:07 +0300

    add js

--- js/xiaomi_13_ultra.js
+++ js/xiaomi_13_ultra.js
@@ -108,9 +108,23 @@ function color() {
 
 //color();
 
-//document.querySelector("p").onclick = color;
+const chk = document.getElementById("chk");
 
-let p = document.querySelectorAll("p");
-p.forEach(function (element) {
-  element.onclick = color;
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
 });
